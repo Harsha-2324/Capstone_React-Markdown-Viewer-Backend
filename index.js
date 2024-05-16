d934b8e0ca53f45b16085411c3fa26b11b206182
Harsha-2324 <rharshva@gmail.com>
2024-05-16 21:02:58 +0530

    Capstone Commit

---
+++ index.js
@@ -0,0 +1,38 @@
+import express from "express";
+import { MongoClient } from "mongodb";
+import * as dotenv from "dotenv";
+import cors from "cors";
+import playersRouter from "./routes/players.route.js";
+import userRouter from "./routes/user.route.js";
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT;
+const MONGO_URI = process.env.MONGODB_URI;
+const client = new MongoClient(MONGO_URI);
+await client.connect();
+console.log("MongoDB is Connected..!");
+
+export { client };
+
+async function startServer() {
+  try {
+    await client.connect();
+    console.log("MongoDB is Connected..!");
+
+    app.use(express.json());
+    app.use(cors());
+    app.use('/players', playersRouter);
+    app.use('/users', userRouter);
+
+    app.get("/", function (req, res) {
+      res.send("Markdown App is Working Fine!");
+    });
+
+    app.listen(PORT, () => console.log(`The Server started in: ${PORT} ✨✨`));
+  } catch (error) {
+    console.error("Error connecting to MongoDB:", error);
+  }
+}
+
+startServer();
